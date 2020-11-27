20e6f9a4907f97f8408c295a13aecf62964237de
Denis Cornehl <denis.cornehl@gmail.com>
2020-11-27 09:50:44 +0100

    fix

--- src/restore.ts
+++ src/restore.ts
@@ -1,41 +1,54 @@
 import * as core from '@actions/core'
-// import * as cache from '@actions/cache'
-// import * as utils from './utils'
+import * as cache from '@actions/cache'
+import * as exec from '@actions/exec'
+import * as utils from './utils'
+import * as path from 'path'
 
 async function run(): Promise<void> {
-  core.setFailed('nxxxasfdf')
-  // try {
-  //   const requirement_files = core.getInput('requirement_files', {
-  //     required: true
-  //   })
-  //   const custom_cache_key = core.getInput('custom_cache_key_element', {
-  //     required: true
-  //   })
-  //   const cache_dir: string = utils.pip_cache_directory()
-  //   core.saveState('PIP_CACHE_DIRECTORY', cache_dir)
-  //   core.setOutput('pip-cache-directory', cache_dir)
-  //   const cache_key: string = await utils.cache_key(
-  //     requirement_files,
-  //     custom_cache_key
-  //   )
-  //   core.saveState('PIP_CACHE_KEY', cache_key)
-  //   core.info(`cache key: ${cache_key}`)
-  //   core.info(`directory to cache: ${cache_dir}`)
-  //   const matched_key = await cache.restoreCache([cache_dir], cache_key, [
-  //     utils.restore_key(custom_cache_key)
-  //   ])
-  //   if (!matched_key) {
-  //     core.info('Cache not found')
-  //     core.setOutput('cache-hit', false.toString())
-  //     return
-  //   }
-  //   const isExactKeyMatch = matched_key === cache_key
-  //   core.saveState('PIP_CACHE_MATCHED_KEY', matched_key)
-  //   core.setOutput('cache-hit', isExactKeyMatch.toString())
-  //   core.info(`Cache restored from key: ${matched_key}`)
-  // } catch (error) {
-  //   core.setFailed(error.message)
-  // }
+  try {
+    const requirement_files = core.getInput('requirement_files', {
+      required: true
+    })
+    const custom_cache_key = core.getInput('custom_cache_key_element', {
+      required: true
+    })
+
+    const virtualenv_dir = await utils.virtualenv_directory()
+    core.saveState('VIRTUALENV_DIRECTORY', virtualenv_dir)
+    core.setOutput('virtualenv-directory', virtualenv_dir)
+
+    const cache_key: string = await utils.cache_key(
+      requirement_files,
+      custom_cache_key
+    )
+
+    core.saveState('VIRTUALENV_CACHE_KEY', cache_key)
+    core.info(`cache key: ${cache_key}`)
+    core.info(`directory to cache: ${virtualenv_dir}`)
+
+    const matched_key = await cache.restoreCache(
+      [virtualenv_dir],
+      cache_key,
+      []
+    )
+    core.saveState('VIRTUALENV_CACHE_MATCHED_KEY', matched_key)
+
+    if (!matched_key) {
+      core.info('Cache not found. creating new virtualenv')
+      core.setOutput('cache-hit', false.toString())
+
+      await exec.exec('python', ['-m', 'venv', virtualenv_dir])
+    } else {
+      core.info(`Cache restored from key: ${matched_key}`)
+      core.setOutput('cache-hit', true.toString())
+    }
+
+    // do what venv/bin/activate normally does
+    core.exportVariable('VIRTUAL_ENV', virtualenv_dir)
+    core.addPath(`${virtualenv_dir}${path.sep}bin`)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
 }
 
 run()
